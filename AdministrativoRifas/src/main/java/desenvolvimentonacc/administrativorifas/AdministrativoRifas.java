70af2105dad81c9262e863d5e02d0ea53188387e
Soulzin01 <112558190+Soulzin01@users.noreply.github.com>
2023-03-14 21:59:41 -0300

    commit proj start

---
+++ AdministrativoRifas/src/main/java/desenvolvimentonacc/administrativorifas/AdministrativoRifas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package desenvolvimentonacc.administrativorifas;
+
+/**
+ *
+ * @author alves
+ */
+public class AdministrativoRifas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
